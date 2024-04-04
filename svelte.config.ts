424496a686f9d52173e3d2aeda3114b9d4ad0f25
Raymond Fleming <r@ymond.wang>
2024-04-04 01:11:02 -0400

    add static site prerender & paths

--- svelte.config.ts
+++ svelte.config.ts
@@ -3,7 +3,12 @@ import type { Config } from '@sveltejs/kit';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config: Config = {
-  kit: { adapter: adapter() },
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+    },
+  },
   // https://kit.svelte.dev/docs/integrations#preprocessors
   preprocess: vitePreprocess(),
 };
