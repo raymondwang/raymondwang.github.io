6e5085162bec2a3b547de609d00bdc3751c49cb4
Raymond Fleming <r@ymond.wang>
2024-04-06 11:50:02 -0400

    move title into layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,8 @@
 <svelte:head>
   <link rel="preload" as="font" href="/fonts/Satoshi-Variable.woff2" type="font/woff2" />
   <link rel="preload" as="font" href="/fonts/Erode-Variable.woff2" type="font/woff2" />
+
+  <title>r@ymond.wang</title>
 </svelte:head>
 
 <slot />
