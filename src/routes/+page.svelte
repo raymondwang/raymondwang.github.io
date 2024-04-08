0dab72f0d3b91c8b8c9193885ba2a039165570dd
Raymond Fleming <r@ymond.wang>
2024-04-07 23:10:40 -0400

    good serif energy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <main class="container px-8 sm:mx-auto">
-  <p class="mt-40 font-serif sm:mt-60">hey, I'm</p>
-  <h1 class="text-4xl font-bold sm:text-6xl md:text-8xl">Raymond</h1>
-  <h2 class="mt-8 font-serif text-2xl">& this page is ~under construction~</h2>
-  <ul class="mt-4 flex flex-row gap-x-4 font-serif">
+  <p class="mt-40 sm:mt-60">hey, I'm</p>
+  <h1 class="text-6xl font-bold sm:text-8xl">Raymond</h1>
+  <h2 class="mt-8 text-2xl">& this page is ~under construction~</h2>
+  <ul class="mt-4 flex flex-row gap-x-4">
     <li>
       <a class="hover:underline hover:underline-offset-4" href="mailto:r@ymond.wang" target="_blank"
         >Email</a
