e4b095df9c661fd39f3da7bc06db23b1f92f46b2
Raymond Wang <r@ymond.wang>
2024-04-18 22:31:22 -0400

    Tinker with copy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,17 @@
 <main class="mx-auto mt-6 h-screen max-w-prose p-6 font-serif">
   <p>hey, I’m</p>
   <h1 class="text-6xl font-bold sm:text-8xl">Raymond</h1>
-  <p class="mt-4">& I’m passionate about building fun experiences on the web.</p>
+  <p class="mt-4">& I’m passionate about building fun digital experiences.</p>
   <p class="mt-4">
-    I’m currently a Principal Engineer at Atlassian, where I focus on making Trello more delightful
-    for users in tandem with modernizing our frontend technology stack.
+    I’m currently a Principal Engineer at Atlassian, where I focus on making
+    Trello more delightful, and lead efforts to modernize our frontend stack.
   </p>
   <ul class="mt-4 flex flex-row gap-x-4">
     <li>
-      <a class="hover:underline hover:underline-offset-4" href="mailto:r@ymond.wang" target="_blank"
-        >Email</a
+      <a
+        class="hover:underline hover:underline-offset-4"
+        href="mailto:r@ymond.wang"
+        target="_blank">Email</a
       >
     </li>
     <li>
