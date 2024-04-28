2410cf293093242a0fc7d28da19b63d7c87f8c68
Raymond Wang <r@ymond.wang>
2024-04-28 18:45:16 -0400

    install simple-import-sort eslint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,35 @@
 /** @type { import("eslint").Linter.Config } */
 module.exports = {
-	root: true,
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'prettier'
-	],
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	},
-	overrides: [
-		{
-			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser',
-			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
-	]
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+    extraFileExtensions: ['.svelte'],
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+    },
+  ],
 };
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,18 @@
 </script>
 
 <svelte:head>
-  <link rel="preload" as="font" href="/fonts/Satoshi-Variable.woff2" type="font/woff2" />
-  <link rel="preload" as="font" href="/fonts/Erode-Variable.woff2" type="font/woff2" />
+  <link
+    rel="preload"
+    as="font"
+    href="/fonts/Satoshi-Variable.woff2"
+    type="font/woff2"
+  />
+  <link
+    rel="preload"
+    as="font"
+    href="/fonts/Erode-Variable.woff2"
+    type="font/woff2"
+  />
 
   <title>r@ymond.wang</title>
 </svelte:head>
