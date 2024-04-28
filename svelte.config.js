b3ef91970db848ea117f98fec300d820ea941c84
Raymond Wang <r@ymond.wang>
2024-04-28 18:48:31 -0400

    Set up mdsvex config

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+  extensions: ['.svx'],
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-  },
-  // https://kit.svelte.dev/docs/integrations#preprocessors
-  preprocess: vitePreprocess(),
+  extensions: ['.svelte', '.md'],
+  kit: { adapter: adapter() },
+  preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 };
 
 export default config;
